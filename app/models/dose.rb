80e3ec0890fefa2fe68e37fcc7c8012e1a5d0bcf
MartinLemiere <mlemiere@norauto.fr>
2018-05-24 12:30:42 +0200

    Skeleton of application mister cocktail

---
+++ app/models/dose.rb
@@ -0,0 +1,10 @@
+class Dose < ApplicationRecord
+
+  validates :description, presence: true
+  validates :cocktail_id, presence: true
+  validates :ingredient_id, presence: true
+  validates_uniqueness_of :cocktail_id, :scope => [:ingredient_id]
+  belongs_to :ingredient
+  belongs_to :cocktail
+
+end
